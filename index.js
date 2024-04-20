8f2d4cfc8cc84be2e30574ab730fbcc11853dd8c
Sailwayfun <liaoleon000513@gmail.com>
2024-04-20 21:45:32 +0800

    feature: fetch Pokemon REST api

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
+const API_ENDPOINT = "https://pokeapi.co/api/v2/";
 
-console.log("sail cli");
+async function printFirstFiveMoves(pokemonName) {
+    const response = await fetch(`${API_ENDPOINT}/pokemon/${pokemonName}`);
+    const data = await response.json();
+
+    const firstFiveMoves = data.moves.map(({ move }) => move.name).slice(0, 5);
+    console.log(firstFiveMoves);
+}
+
+printFirstFiveMoves("charmander");
